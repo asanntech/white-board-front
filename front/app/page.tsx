00e1bd9d7b2cb10277c79d1c368680f94ac0b079
asanntech <asanoyuuki0523@gmail.com>
2025-07-15 22:42:30 +0900

    feat: loginUrl

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,7 +1,11 @@
-'use client'
-
-import { signIn } from 'next-auth/react'
+const loginUrl =
+  `${process.env.NEXT_PUBLIC_COGNITO_DOMAIN}/oauth2/authorize` +
+  `?client_id=${process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID}` +
+  `&response_type=code` +
+  `&scope=openid+email+profile` +
+  `&redirect_uri=${process.env.NEXT_PUBLIC_COGNITO_REDIRECT_URI}` +
+  `&lang=ja`
 
 export default function Home() {
-  return <button onClick={() => signIn('google')}>Sign in with Google</button>
+  return <a href={loginUrl}>Sign in with Cognito</a>
 }
