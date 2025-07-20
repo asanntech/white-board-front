56f3b4df8bc5721eb2a729453af27b556a553a41
asanntech <asanoyuuki0523@gmail.com>
2025-07-20 10:22:08 +0900

    refactor: server action

--- front/app/room/[id]/page.tsx
+++ front/app/room/[id]/page.tsx
@@ -1,32 +1,12 @@
-'use client'
+import { signOutAction } from '@/actions/'
 
-import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
-import { useSession, signOut } from 'next-auth/react'
-
-export default function RoomPage() {
-  const router = useRouter()
-
-  const { data: session, status } = useSession()
-
-  useEffect(() => {
-    if (status === 'unauthenticated') {
-      return router.push('/')
-    }
-  }, [router, status])
-
-  if (status === 'loading') {
-    return <p>Loading...</p>
-  }
-
-  if (status === 'unauthenticated') {
-    return <></>
-  }
-
+export default async function RoomPage() {
   return (
     <div>
-      <p>{session?.user?.email}</p>
-      <button onClick={() => signOut()}>Sign out</button>
+      <p>Hello!</p>
+      <form action={signOutAction}>
+        <button type="submit">Sign Out</button>
+      </form>
     </div>
   )
 }
