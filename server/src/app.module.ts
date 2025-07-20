0b49f794f455b3084ae02c5dbe7547f378827be3
asanntech <asanoyuuki0523@gmail.com>
2025-07-20 20:46:24 +0900

    feat: import AuthModule

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot(), // 環境変数の設定を有効にする
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
