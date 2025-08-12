6f88a98612adc599e4d1760d5a00bcc444398656
asanntech <asanoyuuki0523@gmail.com>
2025-08-12 17:05:10 +0900

    refactor: TOKEN

--- src/open-api/index.ts
+++ src/open-api/index.ts
@@ -1,8 +1,15 @@
 import { ApiClient } from './api-client'
+import { STORAGE_KEYS } from '@/shared/utils'
 
 export const apiClient = new ApiClient({
-  BASE: 'http://localhost:4000',
-  TOKEN: process.env.NEXT_PUBLIC_API_TOKEN,
+  BASE: process.env.NEXT_PUBLIC_API_BASE_URL ?? 'http://localhost:4000',
+  TOKEN: () => {
+    const tokenStr = localStorage.getItem(STORAGE_KEYS.TOKEN_STORE)
+    const tokenStore = tokenStr ? JSON.parse(tokenStr) : undefined
+    if (!tokenStore) throw new Error('No token found')
+
+    return Promise.resolve(tokenStore.accessToken)
+  },
   HEADERS: {
     'Content-Type': 'application/json',
   },
