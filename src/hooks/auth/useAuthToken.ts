0e838756e05fcb6a7d951f464066658c6639c996
asanntech <asanoyuuki0523@gmail.com>
2025-08-12 17:10:49 +0900

    refactor: success

--- src/hooks/auth/useAuthToken.ts
+++ src/hooks/auth/useAuthToken.ts
@@ -1,24 +1,27 @@
-import useLocalStorageState from 'use-local-storage-state'
-import { useAuthQuery, UseAuthQueryOptions } from '@/features/auth'
-import { STORAGE_KEYS } from '@/shared/utils'
+import { useEffect } from 'react'
+import { useAuthQuery } from '@/features/auth'
+import { AuthEntity } from '@/features/auth/domain'
+import { useTokenStore } from '@/hooks'
 
 interface UseAuthTokenParams {
   code?: string
 }
 
-export const useAuthToken = ({ code }: UseAuthTokenParams, options: UseAuthQueryOptions) => {
-  const [, setAccessToken] = useLocalStorageState(STORAGE_KEYS.ACCESS_TOKEN, {})
+interface UseAuthTokenOptions {
+  onSuccess?: (data: AuthEntity) => void
+}
 
-  const { data, isLoading } = useAuthQuery(
-    { code },
-    {
-      ...options,
-      onSuccess: (data) => {
-        setAccessToken(data.accessToken)
-        options?.onSuccess?.(data)
-      },
+export const useAuthToken = ({ code }: UseAuthTokenParams, options?: UseAuthTokenOptions) => {
+  const { setTokenStore } = useTokenStore()
+
+  const { data, isLoading, isSuccess, isError } = useAuthQuery({ code })
+
+  useEffect(() => {
+    if (isSuccess) {
+      setTokenStore(data)
+      options?.onSuccess?.(data)
     }
-  )
+  }, [isSuccess, data, options, setTokenStore])
 
-  return { data, isLoading }
+  return { data, isLoading, isError }
 }
