cf2411460d3f2f291ad3c802802f5eaf89983df4
asanntech <asanoyuuki0523@gmail.com>
2025-08-13 12:33:28 +0900

    feat: deleteToken

--- src/features/auth/domain/auth.repository.ts
+++ src/features/auth/domain/auth.repository.ts
@@ -5,4 +5,5 @@ export interface AuthRepository {
   auth(code: string): Promise<AuthEntity>
   verify(idToken: string): Promise<boolean>
   getToken(): Promise<AuthToken>
+  deleteToken(): Promise<void>
 }
