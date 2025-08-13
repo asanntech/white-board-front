15ce7ee6788b9cfd425c1554f4dc545b7e873f83
asanntech <asanoyuuki0523@gmail.com>
2025-08-13 12:19:07 +0900

    feat: getToken

--- src/features/auth/domain/auth.repository.ts
+++ src/features/auth/domain/auth.repository.ts
@@ -1,6 +1,8 @@
 import { AuthEntity } from './auth.entity'
+import { AuthToken } from '@/shared/types'
 
 export interface AuthRepository {
   auth(code: string): Promise<AuthEntity>
   verify(idToken: string): Promise<boolean>
+  getToken(): Promise<AuthToken>
 }
