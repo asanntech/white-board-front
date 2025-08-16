589a05310f18d5540944bf6dfeef69c880865aa4
asanntech <asanoyuuki0523@gmail.com>
2025-08-16 14:07:32 +0900

    feat: lib

---
+++ src/lib/open-api/api-client/models/AuthVerifyResponseDto.ts
@@ -0,0 +1,31 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type AuthVerifyResponseDto = {
+    /**
+     * ユーザーID
+     */
+    sub: string;
+    /**
+     * 姓
+     */
+    lastName: string;
+    /**
+     * 名
+     */
+    firstName: string;
+    /**
+     * メールアドレス
+     */
+    email: string;
+    /**
+     * 有効期限
+     */
+    exp: number;
+    /**
+     * 作成日時
+     */
+    iat: number;
+};
+
