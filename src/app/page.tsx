8be17bd8bb26845c2d47c3466ced5db54eb1ad5a
asanntech <asanoyuuki0523@gmail.com>
2025-08-30 11:34:54 +0900

    refactor: signIn comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,13 +10,13 @@ const WhiteBoard = dynamic(() => import('@/lib/konva').then((mod) => mod.WhiteBo
   ssr: false,
 })
 
-const signInUrl =
-  `${process.env.NEXT_PUBLIC_COGNITO_DOMAIN}/oauth2/authorize` +
-  `?client_id=${process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID}` +
-  `&response_type=code` +
-  `&scope=openid+email+profile` +
-  `&redirect_uri=${process.env.NEXT_PUBLIC_COGNITO_REDIRECT_URI}` +
-  `&lang=ja`
+// const signInUrl =
+//   `${process.env.NEXT_PUBLIC_COGNITO_DOMAIN}/oauth2/authorize` +
+//   `?client_id=${process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID}` +
+//   `&response_type=code` +
+//   `&scope=openid+email+profile` +
+//   `&redirect_uri=${process.env.NEXT_PUBLIC_COGNITO_REDIRECT_URI}` +
+//   `&lang=ja`
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -51,7 +51,7 @@ const Contents = () => {
 
   return (
     <div>
-      <a href={signInUrl}>Sign in with Cognito</a>
+      {/* <a href={signInUrl}>Sign in with Cognito</a> */}
       <WhiteBoard />
     </div>
   )
