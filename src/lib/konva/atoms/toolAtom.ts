ff2356ebc6b150e67f131c4cbf731b582d17e530
asanntech <asanoyuuki0523@gmail.com>
2025-09-10 21:55:48 +0900

    refactor: control Konva.Node state

--- src/lib/konva/atoms/toolAtom.ts
+++ src/lib/konva/atoms/toolAtom.ts
@@ -1,5 +1,6 @@
 import { atom } from 'jotai'
-import { Tool, DrawingObject } from '../types'
+import Konva from 'konva'
+import { Tool } from '../types'
 
 // ツール状態
 export const toolAtom = atom<Tool>('select')
@@ -9,9 +10,9 @@ const MAX_HISTORY_SIZE = 50
 
 // 履歴の状態を管理するatom
 export const historyAtom = atom<{
-  past: DrawingObject[][]
-  present: DrawingObject[]
-  future: DrawingObject[][]
+  past: Konva.Node[][]
+  present: Konva.Node[]
+  future: Konva.Node[][]
 }>({
   past: [],
   present: [],
@@ -19,17 +20,45 @@ export const historyAtom = atom<{
 })
 
 // 現在の描画オブジェクトを取得
-export const currentDrawingObjectsAtom = atom((get) => get(historyAtom).present)
+export const currentNodesAtom = atom((get) => get(historyAtom).present)
+
+// Lineオブジェクトを取得
+export const lineNodesAtom = atom((get) => {
+  const nodes = get(historyAtom).present.filter((node) => node instanceof Konva.Line)
+  const uniqueNodes = new Map()
+
+  // 後のノードで上書きするために逆順で処理
+  nodes.reverse().forEach((node) => {
+    if (!uniqueNodes.has(node.id())) {
+      uniqueNodes.set(node.id(), node)
+    }
+  })
+  return Array.from(uniqueNodes.values()).reverse()
+})
 
 // 新しい状態を履歴に追加
-export const pushToHistoryAtom = atom(null, (get, set, newState: DrawingObject[]) => {
+export const pushToHistoryAtom = atom(null, (get, set, newState: Konva.Node) => {
   const current = get(historyAtom)
   const newHistory = {
     past: [...current.past, current.present].slice(-MAX_HISTORY_SIZE),
-    present: newState,
+    present: [...current.present, newState],
     future: [], // 新しい状態を追加したら、futureはクリア
   }
   set(historyAtom, newHistory)
+})
+
+// Lineオブジェクトに座標を追加
+export const addPointsToLineAtom = atom(null, (get, set, id: string, points: number[]) => {
+  const current = get(historyAtom)
+
+  const newPresent = current.present.map((node) => {
+    if (node.id() !== id) return node
+    const line = node as Konva.Line
+    line.points([...line.points(), ...points])
+    return line
+  })
+
+  set(historyAtom, { ...current, present: newPresent })
 })
 
 // undo操作
@@ -43,6 +72,7 @@ export const undoAtom = atom(null, (get, set) => {
     present: previous,
     future: [current.present, ...current.future],
   }
+
   set(historyAtom, newHistory)
 })
 
@@ -57,6 +87,7 @@ export const redoAtom = atom(null, (get, set) => {
     present: next,
     future: current.future.slice(1),
   }
+
   set(historyAtom, newHistory)
 })
 
