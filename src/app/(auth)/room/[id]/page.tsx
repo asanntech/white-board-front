98a0c4abca5fc6e97c5a460c2c161523d7366447
asanntech <asanoyuuki0523@gmail.com>
2025-09-13 12:44:03 +0900

    feat: SocketProvider

--- src/app/(auth)/room/[id]/page.tsx
+++ src/app/(auth)/room/[id]/page.tsx
@@ -1,26 +1,41 @@
 'use client'
 
-import { useRouter } from 'next/navigation'
-import { useDeleteTokenMutation } from '@/features/auth'
+import dynamic from 'next/dynamic'
+import { useRouter, useParams } from 'next/navigation'
+// import { useDeleteTokenMutation } from '@/features/auth'
+import { SocketProvider } from '@/lib/konva/components'
 
-const logoutUrl =
-  `${process.env.NEXT_PUBLIC_COGNITO_DOMAIN}/logout` +
-  `?client_id=${process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID}` +
-  `&logout_uri=${process.env.NEXT_PUBLIC_LOGOUT_REDIRECT_URI}`
+// const logoutUrl =
+//   `${process.env.NEXT_PUBLIC_COGNITO_DOMAIN}/logout` +
+//   `?client_id=${process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID}` +
+//   `&logout_uri=${process.env.NEXT_PUBLIC_LOGOUT_REDIRECT_URI}`
+
+const WhiteBoard = dynamic(() => import('@/lib/konva').then((mod) => mod.WhiteBoard), {
+  ssr: false,
+})
 
 export default function RoomPage() {
-  const router = useRouter()
+  // const { mutate: deleteTokenMutate } = useDeleteTokenMutation({
+  //   onSuccess: () => {
+  //     router.replace(logoutUrl)
+  //   },
+  // })
 
-  const { mutate: deleteTokenMutate } = useDeleteTokenMutation({
-    onSuccess: () => {
-      router.replace(logoutUrl)
-    },
-  })
+  const router = useRouter()
+  const params = useParams()
+
+  if (!params?.id) {
+    router.replace('/')
+    return
+  }
 
   return (
-    <div>
-      <p>Hello!</p>
-      <div onClick={() => deleteTokenMutate()}>Sign Out</div>
-    </div>
+    // <div>
+    //   <p>Hello!</p>
+    //   <div onClick={() => deleteTokenMutate()}>Sign Out</div>
+    // </div>
+    <SocketProvider roomId={params.id as string}>
+      <WhiteBoard />
+    </SocketProvider>
   )
 }
