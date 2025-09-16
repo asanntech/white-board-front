b1a57165eab2d55e9ba708f7f85467584adf208d
asanntech <asanoyuuki0523@gmail.com>
2025-09-16 20:07:54 +0900

    feat: create room

--- src/app/(auth)/room/[id]/page.tsx
+++ src/app/(auth)/room/[id]/page.tsx
@@ -7,12 +7,14 @@ import { isReadyCanvasAtom } from '@/lib/konva/atoms'
 import { SocketProvider } from '@/lib/konva/components'
 import { signOutUrl } from '@/shared/constants'
 import { useAtomValue } from 'jotai'
+import { apiClient } from '@/lib/open-api'
+import { userAtom } from '@/atoms'
 
 const WhiteBoard = dynamic(() => import('@/lib/konva').then((mod) => mod.WhiteBoard), {
   ssr: false,
 })
 
-export default function RoomPage() {
+export default function MyRoomPage() {
   const { mutate: deleteTokenMutate } = useDeleteTokenMutation({
     onSuccess: () => {
       router.replace(signOutUrl)
@@ -20,9 +22,21 @@ export default function RoomPage() {
   })
 
   const isReadyCanvas = useAtomValue(isReadyCanvasAtom)
+  const user = useAtomValue(userAtom)
 
   const router = useRouter()
   const params = useParams()
+
+  const createRoom = () => {
+    if (!user?.id) return
+
+    apiClient.rooms.createRoom({
+      requestBody: {
+        name: 'My Room',
+        createdBy: user.id,
+      },
+    })
+  }
 
   if (!params?.id) {
     router.replace('/')
@@ -33,12 +47,20 @@ export default function RoomPage() {
     <SocketProvider roomId={params.id as string}>
       <div className="relative">
         {isReadyCanvas && (
-          <button
-            className="absolute top-5 left-1/2 -translate-x-1/2 bg-emerald-500 w-30 px-3 py-1 shadow-2xl rounded-2xl text-white text-center font-bold cursor-pointer hover:opacity-70 z-1"
-            onClick={() => deleteTokenMutate()}
-          >
-            Sign Out
-          </button>
+          <div className="absolute top-5 left-1/2 -translate-x-1/2 z-1 flex gap-10">
+            <button
+              className="bg-emerald-500 w-30 px-3 py-1 shadow-2xl rounded-2xl text-white text-center font-bold cursor-pointer hover:opacity-70"
+              onClick={() => deleteTokenMutate()}
+            >
+              Sign Out
+            </button>
+            <button
+              className="bg-blue-500 w-50 px-3 py-1 shadow-2xl rounded-2xl text-white text-center font-bold cursor-pointer hover:opacity-70"
+              onClick={createRoom}
+            >
+              Create Room
+            </button>
+          </div>
         )}
         <WhiteBoard />
       </div>
