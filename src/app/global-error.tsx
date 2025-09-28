200a49ca5e02f0029d2db3defd8ec808e927ce40
asanntech <asanoyuuki0523@gmail.com>
2025-09-28 20:36:22 +0900

    feat: GlobalError

---
+++ src/app/global-error.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import * as Sentry from '@sentry/nextjs'
+import { useEffect } from 'react'
+import { ErrorPage } from '@/components/error'
+import { BackHomeButton } from '@/components/button'
+
+export default function GlobalError({ error }: { error: Error & { digest?: string } }) {
+  useEffect(() => {
+    Sentry.captureException(error)
+  }, [error])
+
+  return (
+    <html>
+      <body>
+        <ErrorPage
+          status="!"
+          title="不明なエラーが発生しました"
+          message="再度お試しください。"
+          primaryAction={<BackHomeButton />}
+        />
+      </body>
+    </html>
+  )
+}
