74193136a6cf80d0c9f753f111b50a704436ead7
asanntech <asanoyuuki0523@gmail.com>
2025-09-29 18:52:03 +0900

    chore: invalid sentry local

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,13 +1,18 @@
-import * as Sentry from '@sentry/nextjs';
+import * as Sentry from '@sentry/nextjs'
 
 export async function register() {
+  // local開発時はSentryの設定を読み込まない
+  if (process.env.NODE_ENV === 'development') {
+    return
+  }
+
   if (process.env.NEXT_RUNTIME === 'nodejs') {
-    await import('../sentry.server.config');
+    await import('../sentry.server.config')
   }
 
   if (process.env.NEXT_RUNTIME === 'edge') {
-    await import('../sentry.edge.config');
+    await import('../sentry.edge.config')
   }
 }
 
-export const onRequestError = Sentry.captureRequestError;
+export const onRequestError = Sentry.captureRequestError
