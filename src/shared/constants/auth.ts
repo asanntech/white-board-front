237c57bdcf6e9385fcd92fb75df9883d5bb3ea1e
asanntech <asanoyuuki0523@gmail.com>
2025-09-30 08:26:13 +0900

    refactor: redirect_uri

--- src/shared/constants/auth.ts
+++ src/shared/constants/auth.ts
@@ -3,8 +3,8 @@ export const signInUrl =
   `?client_id=${process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID}` +
   `&response_type=code` +
   `&scope=openid+email+profile` +
-  `&redirect_uri=${process.env.NEXT_PUBLIC_COGNITO_REDIRECT_URI}` +
-  `&lang=ja`
+  `&lang=ja` +
+  `&redirect_uri=${process.env.NEXT_PUBLIC_COGNITO_REDIRECT_URI}`
 
 export const signOutUrl =
   `${process.env.NEXT_PUBLIC_COGNITO_DOMAIN}/logout` +
