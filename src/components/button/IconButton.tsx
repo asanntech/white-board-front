280ea886f1042033d98533e2607350cb7b5788ee
asanntech <asanoyuuki0523@gmail.com>
2025-09-30 08:21:04 +0900

    feat: IconButton

---
+++ src/components/button/IconButton.tsx
@@ -0,0 +1,20 @@
+interface IconButtonProps {
+  icon: React.ReactNode
+  active?: boolean
+  onClick?: () => void
+  disabled?: boolean
+}
+
+export const IconButton = ({ icon, active, onClick, disabled = false }: IconButtonProps) => {
+  return (
+    <button
+      className={`rounded-md p-2 ${active ? 'bg-neutral-100' : ''} ${
+        disabled ? 'opacity-30 cursor-not-allowed' : 'cursor-pointer hover:bg-neutral-100'
+      }`}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {icon}
+    </button>
+  )
+}
