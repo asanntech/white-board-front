05b8f90caf33e85f6fd17d64bca173a37f8710ff
asanntech <asanoyuuki0523@gmail.com>
2025-09-30 20:32:53 +0900

    feat: main icon

--- src/components/menu/MainMenu.tsx
+++ src/components/menu/MainMenu.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 interface MainMenuProps {
   signInUrl: string
   onClickFreeTrial: () => void
@@ -7,6 +8,9 @@ export const MainMenu = ({ signInUrl, onClickFreeTrial }: MainMenuProps) => {
   return (
     <section className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-120 w-11/12 bg-white text-center rounded-md p-6 shadow-lg">
       <h1 className="text-4xl font-bold text-emerald-500">White Board</h1>
+      <div className="mt-4 flex justify-center">
+        <Image src="/main.svg" width={100} height={100} alt="white board logo" priority />
+      </div>
       <div className="flex flex-col gap-4 mt-5">
         <button
           onClick={onClickFreeTrial}
