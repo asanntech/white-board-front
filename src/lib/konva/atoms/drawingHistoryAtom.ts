e726fd6ca45aa76a4c47956877a439e1c93e794e
asanntech <asanoyuuki0523@gmail.com>
2025-10-04 10:09:25 +0900

    feat: Added initialization and update functions for drawing history management

--- src/lib/konva/atoms/drawingHistoryAtom.ts
+++ src/lib/konva/atoms/drawingHistoryAtom.ts
@@ -30,35 +30,42 @@ export const lineNodesAtom = atom<Konva.Line[]>((get) => {
   return Array.from(uniqueNodes.values()).reverse()
 })
 
+export const initHistoryAtom = atom(null, (get, set, initialState: Konva.Node[]) => {
+  set(historyAtom, {
+    past: [],
+    present: initialState,
+    future: [],
+  })
+})
+
 // 新しい状態を履歴に追加
-export const pushToHistoryAtom = atom(null, (get, set, newState: Konva.Node | Konva.Node[]) => {
+export const pushToHistoryAtom = atom(null, (get, set, newState: Konva.Node | Konva.Node[], clearHistory?: boolean) => {
   const current = get(historyAtom)
 
   const newPresent = Array.isArray(newState) ? newState : [newState]
 
   const newHistory = {
-    past: current.present.length > 0 ? [...current.past, current.present].slice(-MAX_HISTORY_SIZE) : current.past,
+    past: !clearHistory ? [...current.past, current.present].slice(-MAX_HISTORY_SIZE) : [],
     present: [...current.present, ...newPresent],
     future: [], // 新しい状態を追加したら、futureはクリア
   }
   set(historyAtom, newHistory)
 })
 
-// Lineオブジェクトに座標を追加
-export const addPointsToLineAtom = atom(null, (get, set, id: string, points: number[]) => {
+export const updateLineAtom = atom(null, (get, set, updateNodes: Konva.Node[]) => {
   const current = get(historyAtom)
 
   const newPresent = current.present.map((node) => {
-    if (node.id() !== id) return node
-    const line = node as Konva.Line
-    line.points([...line.points(), ...points])
-    return line
+    const updateNode = updateNodes.find((updateNode) => updateNode.id() === node.id())
+    if (updateNode) return updateNode
+    return node
   })
+
   set(historyAtom, { ...current, present: newPresent })
 })
 
 // 現在の描画オブジェクトから指定したIDのオブジェクトを削除
-export const removeLineAtom = atom(null, (get, set, id: string | string[]) => {
+export const removeLineAtom = atom(null, (get, set, id: string | string[], clearHistory?: boolean) => {
   const current = get(historyAtom)
 
   // 単一のIDまたは複数のIDに対応
@@ -66,14 +73,13 @@ export const removeLineAtom = atom(null, (get, set, id: string | string[]) => {
   const newPresent = current.present.filter((node) => !idsToRemove.includes(node.id()))
 
   const newHistory = {
-    past: current.present.length > 0 ? [...current.past, current.present] : current.past,
+    past: !clearHistory ? [...current.past, current.present].slice(-MAX_HISTORY_SIZE) : [],
     present: newPresent,
-    future: [],
+    future: [], // 新しい状態を追加したら、futureはクリア
   }
   set(historyAtom, newHistory)
 })
 
-// undo操作
 export const undoAtom = atom(null, (get, set) => {
   const current = get(historyAtom)
   if (current.past.length === 0) return
