f8d477b3bee774e454279b00a5a3548353e2c56e
asanntech <asanoyuuki0523@gmail.com>
2025-10-20 21:42:06 +0900

    refactor: tailwind-merge

--- src/components/toast/Toast.tsx
+++ src/components/toast/Toast.tsx
@@ -1,3 +1,4 @@
+import { twJoin } from 'tailwind-merge'
 interface ToastProps {
   showToast: boolean
   toastMessage: string
@@ -7,9 +8,10 @@ export const Toast = ({ showToast, toastMessage }: ToastProps) => {
   return (
     <div
       aria-live="polite"
-      className={`fixed left-1/2 -translate-x-1/2 top-6 z-1 transition-all duration-300 ${
+      className={twJoin(
+        'fixed left-1/2 -translate-x-1/2 top-6 z-1 transition-all duration-300',
         showToast ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-2 pointer-events-none'
-      }`}
+      )}
     >
       <div className="bg-gray-900 text-white text-sm px-4 py-2 rounded shadow-md">{toastMessage}</div>
     </div>
